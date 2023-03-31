76a53fcd7a91b493f14df2a002a3cb5bea2425de
PlantWasTaken <69759382+PlantWasTaken@users.noreply.github.com>
2023-03-31 15:18:51 +0200

    Update fish.py

--- fish.py
+++ fish.py
@@ -9,11 +9,11 @@ keyboard = Controller()
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files (x86)\Tesseract-OCR\tesseract.exe'
 x1,y1 = (384, 948)
 x2,y2 = (500, 981)
-print(pytesseract.image_to_string('cap.png'))
 
 t.sleep(5)
 while True:
-    keyboard.type('/fish ')
+    keyboard.type('/fish')
+    keyboard.press(Key.space)
     keyboard.press(Key.enter)
     t.sleep(2)
     #chapta detection / anyother detection
@@ -24,12 +24,34 @@ while True:
     img_diff = Image.new("RGBA", img_a.size)
     mismatch = pixelmatch(img_a, img_b, img_diff, includeAA=True)
 
+
     if(mismatch != 0):
-        keyboard.type('ABORT')
-        keyboard.press(Key.enter)
-        exit()
+        #capcha
+        x1,y1 = (399, 877) 
+        x2, y2 = (562, 911) 
+        capcha = ImageGrab.grab(bbox=(x1,y1,x2,y2))
+        capcha.save("cap.png")
+        cap_temp = pytesseract.image_to_string('cap.png')
+        cap = ""
+        for i in cap_temp:
+            print(i)
+            if(i == " "):
+                pass
+            else:
+                cap = cap + i
+
+        #if error or captcha
+        if(cap == ''):
+            pass
+        else:
+            keyboard.type('/verify')   
+            keyboard.press(Key.space)   
+            keyboard.type(cap)
+            keyboard.press(Key.enter)
+        
+        t.sleep(3)
     else:
-        t.sleep(1)
+        t.sleep(1.25)
         pass
 
 
