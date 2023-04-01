0272e9e9b0ab93e0da09abd63e90f12ce8610f06
PlantWasTaken <69759382+PlantWasTaken@users.noreply.github.com>
2023-04-01 13:48:00 +0200

    Update fish.py

--- fish.py
+++ fish.py
@@ -7,25 +7,59 @@ import time as t
 
 keyboard = Controller()
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files (x86)\Tesseract-OCR\tesseract.exe'
-x1,y1 = (384, 948)
-x2,y2 = (500, 981)
+count = 0
 
-t.sleep(5)
-while True:
+t.sleep(1) #5
+
+def fish():
+    global count
     keyboard.type('/fish')
+    t.sleep(0.1)
     keyboard.press(Key.space)
+    t.sleep(0.1)
     keyboard.press(Key.enter)
+    print(count)
+    count = count + 1
+
+def fisherror():
+    img_a = Image.open("fisherror.png")
+    img_b = Image.open("TestFish.png")
+    img_diff = Image.new("RGBA", img_a.size)
+    mismatch = pixelmatch(img_a, img_b, img_diff, includeAA=True)
+    return mismatch
+
+def timererror():
+    img_a = Image.open("timer.png")
+    img_b = Image.open("TestFish.png")
+    img_diff = Image.new("RGBA", img_a.size)
+    mismatch = pixelmatch(img_a, img_b, img_diff, includeAA=True)
+    return mismatch
+
+def senderror():
+    img_a = Image.open("send.png")
+    img_b = Image.open("TestFish.png")
+    img_diff = Image.new("RGBA", img_a.size)
+    mismatch = pixelmatch(img_a, img_b, img_diff, includeAA=True)
+    return mismatch
+
+while True:
+    x1,y1 = (384, 948) #fish 
+    x2,y2 = (500, 981) #fish
+
+    fish()
+
     t.sleep(2)
-    #chapta detection / anyother detection
+    #chapta detection / anyother detection / error detection
     igmr = ImageGrab.grab(bbox=(x1,y1,x2,y2))
     igmr.save("TestFish.png")
+
     img_a = Image.open("FISH.png")
     img_b = Image.open("TestFish.png")
     img_diff = Image.new("RGBA", img_a.size)
     mismatch = pixelmatch(img_a, img_b, img_diff, includeAA=True)
 
-
-    if(mismatch != 0):
+    t.sleep(1)
+    if(mismatch != 0 and mismatch != 115):
         #capcha
         x1,y1 = (399, 877) 
         x2, y2 = (562, 911) 
@@ -34,24 +68,27 @@ while True:
         cap_temp = pytesseract.image_to_string('cap.png')
         cap = ""
         for i in cap_temp:
-            print(i)
             if(i == " "):
                 pass
             else:
                 cap = cap + i
 
         #if error or captcha
-        if(cap == ''):
-            pass
-        else:
-            keyboard.type('/verify')   
-            keyboard.press(Key.space)   
+        if(len(cap) == 6):
+            print(cap)
+            t.sleep(0.2)
+            keyboard.type('/verify')
+            t.sleep(0.2)
+            keyboard.press(Key.space)
+            t.sleep(0.2) 
             keyboard.type(cap)
+            t.sleep(0.2)
             keyboard.press(Key.enter)
-        
+        else:
+            t.sleep(1)
+            fish()
         t.sleep(3)
     else:
-        t.sleep(1.25)
+        t.sleep(0.7) #3.5s total + 0.2 dleay
+        t.sleep(0.3)
         pass
-
-
