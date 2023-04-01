49c76c5eda4cac05fe804aa5e76a30d6377d482f
PlantWasTaken <69759382+PlantWasTaken@users.noreply.github.com>
2023-04-01 14:58:56 +0200

    Update fish.py

--- fish.py
+++ fish.py
@@ -8,87 +8,68 @@ import time as t
 keyboard = Controller()
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files (x86)\Tesseract-OCR\tesseract.exe'
 count = 0
-
-t.sleep(1) #5
+x1,y1 = (394, 482) #screen
+x2,y2 = (1061, 955) #screen
 
 def fish():
-    global count
     keyboard.type('/fish')
     t.sleep(0.1)
     keyboard.press(Key.space)
     t.sleep(0.1)
     keyboard.press(Key.enter)
-    print(count)
-    count = count + 1
 
-def fisherror():
-    img_a = Image.open("fisherror.png")
-    img_b = Image.open("TestFish.png")
-    img_diff = Image.new("RGBA", img_a.size)
-    mismatch = pixelmatch(img_a, img_b, img_diff, includeAA=True)
-    return mismatch
+def cap():
+    def type(captcha):
+        keyboard.type('/verify')
+        t.sleep(0.3)
+        keyboard.press(Key.space)
+        t.sleep(0.3) 
+        keyboard.type(captcha)
+        t.sleep(0.3)
+        keyboard.press(Key.enter)
 
-def timererror():
-    img_a = Image.open("timer.png")
-    img_b = Image.open("TestFish.png")
-    img_diff = Image.new("RGBA", img_a.size)
-    mismatch = pixelmatch(img_a, img_b, img_diff, includeAA=True)
-    return mismatch
+    c_x1,c_y1 = (443, 795)
+    c_x2,c_y2 = (623, 832)
+    igmr = ImageGrab.grab(bbox=(c_x1,c_y1,c_x2,c_y2))
+    igmr.save("cap.png")
+    captcha_temp = (pytesseract.image_to_string('cap.png'))
+    captcha = ""
 
-def senderror():
-    img_a = Image.open("send.png")
-    img_b = Image.open("TestFish.png")
-    img_diff = Image.new("RGBA", img_a.size)
-    mismatch = pixelmatch(img_a, img_b, img_diff, includeAA=True)
-    return mismatch
+    for i in captcha_temp:
+        if(i == " " or i == "" or i == "\n"):
+            pass
+        else:
+            captcha = captcha + i
+
+    type(captcha)
+
+    igmr = ImageGrab.grab(bbox=(x1,y1,x2,y2))
+    igmr.save("Capture.png")
+    text = (pytesseract.image_to_string('capture.png'))
+
+    Done = False
+
+    while Done == False:
+        if("You may now continue." in text):
+            Done = True
+            pass
+        else:
+            type(captcha)
+            t.sleep(5)
 
 while True:
-    x1,y1 = (384, 948) #fish 
-    x2,y2 = (500, 981) #fish
-
-    fish()
-
-    t.sleep(2)
-    #chapta detection / anyother detection / error detection
     igmr = ImageGrab.grab(bbox=(x1,y1,x2,y2))
-    igmr.save("TestFish.png")
+    igmr.save("Capture.png")
+    text = (pytesseract.image_to_string('capture.png'))
 
-    img_a = Image.open("FISH.png")
-    img_b = Image.open("TestFish.png")
-    img_diff = Image.new("RGBA", img_a.size)
-    mismatch = pixelmatch(img_a, img_b, img_diff, includeAA=True)
-
-    t.sleep(1)
-    if(mismatch != 0 and mismatch != 115):
-        #capcha
-        x1,y1 = (399, 877) 
-        x2, y2 = (562, 911) 
-        capcha = ImageGrab.grab(bbox=(x1,y1,x2,y2))
-        capcha.save("cap.png")
-        cap_temp = pytesseract.image_to_string('cap.png')
-        cap = ""
-        for i in cap_temp:
-            if(i == " "):
-                pass
-            else:
-                cap = cap + i
-
-        #if error or captcha
-        if(len(cap) == 6):
-            print(cap)
-            t.sleep(0.2)
-            keyboard.type('/verify')
-            t.sleep(0.2)
-            keyboard.press(Key.space)
-            t.sleep(0.2) 
-            keyboard.type(cap)
-            t.sleep(0.2)
-            keyboard.press(Key.enter)
-        else:
-            t.sleep(1)
-            fish()
-        t.sleep(3)
+    if("You caught:" in text):
+        print("fish")
+        fish()
+    elif("Anti-bot" in text):
+        print("cap")
+        cap()
     else:
-        t.sleep(0.7) #3.5s total + 0.2 dleay
-        t.sleep(0.3)
-        pass
+        print("error")
+        exit()
+
+    t.sleep(3.7)
